714e127431d3d908a0ac366b454ef4c3731c3ccd
rudderbucky <anandkwork7@gmail.com>
2019-10-19 10:46:35 -0500

    first commit

---
+++ Carbonics-main/Carbonics-main/Carbonics-main/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Carbonics_main
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
