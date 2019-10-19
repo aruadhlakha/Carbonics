74f3113da1142e4a0fddfc50fe21828cb091d3a3
ARYANADHLAKHA <AADHLAKHA@WISC.EDU>
2019-10-19 12:54:36 -0500

    Aryan's Task class implementation

---
+++ carbonics/carbonics/carbonics/Task.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace carbonics
+{
+    class Task
+    {
+        String desc;
+        bool check=false;    
+        bool checkComp()
+        {
+            return check; 
+        }
+        int SwipeRight()
+        {
+            check = true;
+            return 0;
+        }
+        int swipeLeft()
+        {
+            check = false;
+            return 0;
+        }
+    }
+}
