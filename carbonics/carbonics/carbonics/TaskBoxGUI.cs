3d872f704b7a899f4a4d823ec56aca7424c9ff40
ARYANADHLAKHA <AADHLAKHA@WISC.EDU>
2019-10-20 03:54:04 -0500

    lol

--- carbonics/carbonics/carbonics/TaskBoxGUI.cs
+++ carbonics/carbonics/carbonics/TaskBoxGUI.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
 
 namespace carbonics
 {
     class TaskBoxGUI : Grid
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "sl8xzZIXaxap45WOhfBWX50q9IM0nmziqjtaYwtt",
+            BasePath = "https://xamarinapp-256505.firebaseio.com/"
+        };
+        IFirebaseClient client;
         public int experience = 20;
+
         public TaskBoxGUI(string t)
         {
+            client = new FireSharp.FirebaseClient(config);
+            data_to_upload ={
+                'username': username
+                'XP':xp
+    }
+            client.Push("/MyTestData", data_to_uploa);
             Children.Add(new Label { Text = t, BackgroundColor = Color.Gold, HeightRequest = 60, HorizontalTextAlignment = TextAlignment.Center, VerticalTextAlignment = TextAlignment.Center });
             var fail = new Button { Text = "Not quite", FontSize = 8, BackgroundColor = Color.LightSteelBlue, HeightRequest = 60, WidthRequest = 60, HorizontalOptions = LayoutOptions.Start };
             Children.Add(fail);
@@ -18,7 +35,6 @@ namespace carbonics
             var pass = new Button { Text = "Did it!", FontSize = 8, BackgroundColor = Color.LightSteelBlue, HeightRequest = 60, WidthRequest = 60, HorizontalOptions = LayoutOptions.End };
             Children.Add(pass);
             pass.Clicked += new EventHandler((object sender, EventArgs args) => { Destroy(true); });
-
         }
 
         public TaskBoxGUI(Task task)
